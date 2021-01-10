1e35b7e2e59078c438ccb2893b5b2c23880f3168
LAPTOP-FGPTHUNM\Karan <vijay.chauhan@plutoshift.com>
2021-01-10 17:15:35 -0500

    simple CI-CD pipeline

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "welcome to my website"
+
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0')
